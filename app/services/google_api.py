442ba4de06e261a808393b27954d12e0c60bdd7f
Константин Гашев <gashev1989@yandex.ru>
2023-10-22 15:11:41 +0500

    Code improved

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -3,7 +3,10 @@ from datetime import datetime
 from aiogoogle import Aiogoogle
 from app.core.config import settings
 
+COLUMN_COUNT = 11
 FORMAT = "%Y/%m/%d %H:%M:%S"
+ROW_COUNT = 100
+SHEET_ID = 0
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
@@ -13,16 +16,15 @@ async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
         'properties': {'title': f'Отчет на {now_date_time}',
                        'locale': 'ru_RU'},
         'sheets': [{'properties': {'sheetType': 'GRID',
-                                   'sheetId': 0,
+                                   'sheetId': SHEET_ID,
                                    'title': 'Лист1',
-                                   'gridProperties': {'rowCount': 100,
-                                                      'columnCount': 11}}}]
+                                   'gridProperties': {'rowCount': ROW_COUNT,
+                                                      'columnCount': COLUMN_COUNT}}}]
     }
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
-    spreadsheetid = response['spreadsheetId']
-    return spreadsheetid
+    return response['spreadsheetId']
 
 
 async def set_user_permissions(
@@ -37,7 +39,7 @@ async def set_user_permissions(
         service.permissions.create(
             fileId=spreadsheetid,
             json=permissions_body,
-            fields="id"
+            fields='id'
         ))
 
 
